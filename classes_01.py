e5593c6b4aa1712c9b57b5a17f5a9d1b13e725b5
jltichy <jltichy@gmail.com>
2016-03-24 15:55:25 +0000

    several examples are included

--- classes_01.py
+++ classes_01.py
@@ -31,12 +31,40 @@ x.printHam() # accessing the function created within the base class
 
 # Another example - with a character class:
 
-class Character(object):
-    def_init_(self):
-    self.health = 100
+class Character(object): # create a simple class
+    def __init__(self): # calls the constructor self - notice that there is a space after def and that there are two underscores on either side of init
+        self.health = 100 # create an attribute called health
         
-class Blacksmith(Character):
-    def_init_(self):
-    super(Blacksmith, self)._init_()
-        
+class Blacksmith(Character): # creating inheriting class
+    def __init__(self): # inherit from base class above
+        super(Blacksmith, self).__init__() # super
 
+bs = Blacksmith()
+print bs.health
+
+# the above program prints "100" to the screen
+
+# if we delete the object from this example, the we do get an error because the super function no longer works.
+
+
+# Yet another example - with an additional parameter:
+
+class Char(object): # create a simple class; Character was shortened to Char for this example
+    def __init__(self, name): # added the parameter called name
+        self.health = 100 # create an attribute called health
+        self.name = name # securing "name" to the class
+    def PrintName(self):
+        print self.name
+        
+class Black(Char): # creating inheriting class; Blacksmith was shortened to Black for this example
+    def __init__(self, name, forgeName): # inherit from base class above
+        super(Black, self).__init__(name) # super
+        self.forge = Forge(forgeName)
+        
+class Forge:
+    def __init__(self, forgeName):
+        self.name = forgeName
+    
+BS = Black("Bob", "Rick\'s forge")
+BS.PrintName()
+print BS.forge.name
